a34780055de5bf69ab1e35cd31dd17dd3fa41988
hiraitakaya <hirai-takaya@ed.tmu.ac.jp>
2025-04-09 13:41:46 +0900

    テーブルを作成

--- database/migrations/2025_04_09_044017_create_follows_table.php
+++ database/migrations/2025_04_09_044017_create_follows_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('follows', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('followed_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('follows');
     }
 };
