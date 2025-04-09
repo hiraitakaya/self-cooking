22dafd7c6661ad9246ab7333aa6e1b290a04833f
hiraitakaya <hirai-takaya@ed.tmu.ac.jp>
2025-04-09 16:39:12 +0900

    修正

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // ← ここ重要！
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
